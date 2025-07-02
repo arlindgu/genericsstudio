87f8c58aa3592603fdace34b7f96fc46d98aa7c5
arlindgurgurovci <155858954+arlindgurgurovci@users.noreply.github.com>
2025-07-02 20:29:14 +0200

    fixed naming problem with footer component fixed typo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -24,12 +24,12 @@ export default function ContactPage() {
             <div className="flex flex-col items-center justify-center gap-4 w-full max-w-2xl">
                 <Steps step={0}>You send us an inquiry.</Steps>
                 <Steps step={1}>We review it.</Steps>
-                <Steps step={3}>We get in touch with you.</Steps>
-                <Steps step={4}>We talk about your ideas and vision.</Steps>
-                <Steps step={5}>We send you an offer.</Steps>
-                <Steps step={6}>If you accept the offer, we start building your magical new website.</Steps>
-                <Steps step={7}>You’ll always get support whenever needed or if changes come up.</Steps>
-                <Steps step={8}>We’ll keep in touch and make sure your website stays up to date.</Steps>
+                <Steps step={2}>We get in touch with you.</Steps>
+                <Steps step={3}>We talk about your ideas and vision.</Steps>
+                <Steps step={4}>We send you an offer.</Steps>
+                <Steps step={5}>If you accept the offer, we start building your magical new website.</Steps>
+                <Steps step={6}>You’ll always get support whenever needed or if changes come up.</Steps>
+                <Steps step={7}>We’ll keep in touch and make sure your website stays up to date.</Steps>
             </div>
         </main>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Instrument_Serif, Instrument_Sans } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 const instrumentSerif = Instrument_Serif({
