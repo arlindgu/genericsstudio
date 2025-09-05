c9287bedfffd8a36dafddf254002a76ce18ef012
arlindgurgurovci <155858954+arlindgurgurovci@users.noreply.github.com>
2025-09-05 22:43:37 +0200

    fixed eslint issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import localFont from "next/font/local";
-import { useEffect, useState } from "react";
 
 
 // Import General Sans font using next/font/local
